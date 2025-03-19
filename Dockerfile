3c3492379fd0d67a9fe24e0e0d0442108c86fb82
MironaAloneh <mironaaloneh@gmail.com>
2025-03-19 17:34:43 +0600

    add an uploads dir to docker

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,8 @@ WORKDIR /app
 # Копируем файлы проекта
 COPY . .
 
+RUN mkdir -p /app/uploads
+
 # Устанавливаем зависимости
 RUN pip install --no-cache-dir -r requirements.txt
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 import os
+import cv2
 
 app = FastAPI()
 
@@ -8,8 +9,16 @@ async def upload_file(file: UploadFile = File(...)):
     upload_dir = "uploads"
     if not os.path.exists(upload_dir):
         os.makedirs(upload_dir)
+
     file_location = f"uploads/{file.filename}"
+    os.write
     with open(file_location, "wb") as f:
         f.write(await file.read())  
-    return {"info": f"File saved at {file_location}"}
+    
+    image = cv2.imread(f"{file_location}", cv2.IMREAD_COLOR)
+    if image is not None:
+        print("Изображение успешно загружено!")
+    else:
+        print("Не удалось загрузить изображение.")
+   # return {"info": f"File saved at {file_location}"}
 
