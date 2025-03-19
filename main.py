d1f3fe76b1ad7030e9b513ecaa15f5b29486ce28
MironaAloneh <mironaaloneh@gmail.com>
2025-03-19 21:55:32 +0600

    test saving files to shutil

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.staticfiles import StaticFiles
+import shutil
 import os
-import cv2
 
 app = FastAPI()
 
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+# Endpoint to upload files
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
-    upload_dir = "uploads"
-    if not os.path.exists(upload_dir):
-        os.makedirs(upload_dir)
-    
-    file_location = f"/uploads/{file.filename}"
-    url = os.getenv("REQUEST_URL")
-    image_url = f"{url}{file_location}"
-    print(image_url)
-    img = cv2.imread(image_url, cv2.IMREAD_COLOR)
-    cv2.imwrite( image_url, file )
-    return {"info": f"Image uploaded successfully at {image_url}"}
+    file_location = os.path.join(UPLOAD_DIR, file.filename)
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"filename": file.filename, "url": f"/files/{file.filename}"}
 
-    #file_location = f"uploads/{file.filename}"
-    #os.write
-    #f.write(await file.read())  
-   # return {"info": f"File saved at {file_location}"}
+# Serve uploaded files
+app.mount("/files", StaticFiles(directory=UPLOAD_DIR), name="files")
+
+
+# @app.post("/upload/")
+# async def upload_file(file: UploadFile = File(...)):
+#     upload_dir = "uploads"
+#     if not os.path.exists(upload_dir):
+#         os.makedirs(upload_dir)
+    
+#     file_location = f"/uploads/{file.filename}"
+#     url = os.getenv("REQUEST_URL")
+#     image_url = f"{url}{file_location}"
+#     print(image_url)
+#     img = cv2.imread(image_url, cv2.IMREAD_COLOR)
+#     cv2.imwrite( image_url, file )
+#     return {"info": f"Image uploaded successfully at {image_url}"}
+
+#     #file_location = f"uploads/{file.filename}"
+#     #os.write
+#     #f.write(await file.read())  
+#    # return {"info": f"File saved at {file_location}"}
 
