625296f18dc7f3a58b301a690f9d2bc7b00f02b9
MironaAloneh <mironaaloneh@gmail.com>
2025-03-23 02:07:53 +0600

    delete a result image

--- main.py
+++ main.py
@@ -36,8 +36,6 @@ async def upload_and_analize(file: UploadFile = File(...)):
     results = model(image)
     print(f"this is result: {results}") 
     print(f"Type of results: {type(results)}") 
-    result_image = cv2.imread(results[6], image)
-    print(f"this is a result_image: {result_image}")
     result_path = os.path.join(results.save_dir, results.path)
     print(f"result_path: {result_path}")
     with open(result_path, "rb") as f:
