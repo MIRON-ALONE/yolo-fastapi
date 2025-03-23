f3fc29ecf0a45ebefa60d2b58ba129ed644f7be0
MironaAloneh <mironaaloneh@gmail.com>
2025-03-24 02:15:06 +0600

    settings.update({"datasets_dir": "/app/datasets/coco/images/train2017"})  coco128 -> coco

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from ultralytics import YOLO, settings
 import cv2
 
 app = FastAPI()
-settings.update({"datasets_dir": "/app/datasets/coco128/images/train2017"})
+settings.update({"datasets_dir": "/app/datasets/coco/images/train2017"})
 model = YOLO(model="yolov8n-cls.pt")
 if __name__ == "__main__":
     results = model.train(data="/app/datasets/coco/images/train2017", epochs=100, imgsz=640)
